3b5d0bcaa6f0674909236941b71f4116bf742b98
shaqnawe <shaktishah40@gmail.com>
2023-06-13 11:18:33 -0500

    updated constants file

--- trendmicrovisionone_consts.py
+++ trendmicrovisionone_consts.py
@@ -15,44 +15,37 @@
 API_VER = "v3.0"
 
 # COMMAND NAMES
-ENABLE_USER_ACCOUNT_COMMAND = "trendmicro-visionone-enable-user-account"
-DISABLE_USER_ACCOUNT_COMMAND = "trendmicro-visionone-disable-user-account"
-FORCE_SIGN_OUT_COMMAND = "trendmicro-visionone-force-sign-out"
-FORCE_PASSWORD_RESET_COMMAND = "trendmicro-visionone-force-password-reset"
-ADD_BLOCKLIST_COMMAND = "trendmicro-visionone-add-to-block-list"
-REMOVE_BLOCKLIST_COMMAND = "trendmicro-visionone-remove-from-block-list"
-QUARANTINE_EMAIL_COMMAND = "trendmicro-visionone-quarantine-email-message"
-DELETE_EMAIL_COMMAND = "trendmicro-visionone-delete-email-message"
-ISOLATE_ENDPOINT_COMMAND = "trendmicro-visionone-isolate-endpoint"
-RESTORE_ENDPOINT_COMMAND = "trendmicro-visionone-restore-endpoint-connection"
-TERMINATE_PROCESS_COMMAND = "trendmicro-visionone-terminate-process"
-ADD_EXCEPTION_LIST_COMMAND = "trendmicro-visionone-add-objects-to-exception-list"
-DELETE_EXCEPTION_LIST_COMMAND = (
-    "trendmicro-visionone-delete-objects-from-exception-list"
-)
-ADD_SUSPICIOUS_LIST_COMMAND = "trendmicro-visionone-add-objects-to-suspicious-list"
-DELETE_SUSPICIOUS_LIST_COMMAND = (
-    "trendmicro-visionone-delete-objects-from-suspicious-list"
-)
-GET_SANDBOX_SUBMISSION_STATUS_COMMAND = (
-    "trendmicro-visionone-get-sandbox-submission-status"
-)
-GET_SANDBOX_ANALYSIS_RESULTS_COMMAND = (
-    "trendmicro-visionone-get-sandbox-analysis-results"
-)
-DOWNLOAD_SANDBOX_ANALYSIS_REPORT_COMMAND = (
-    "trendmicro-visionone-download-sandbox-analysis-report"
-)
-DOWNLOAD_INVESTIGATION_PACKAGE_COMMAND = (
-    "trendmicro-visionone-download-investigation-package"
-)
-DOWNLOAD_SUSPICIOUS_OBJECT_LIST_COMMAND = (
-    "trendmicro-visionone-download-suspicious-object-list"
-)
-TEST_CONNECTIVITY_COMMAND = "trendmicro-visionone-test-connectivity"
-COLLECT_FILE_COMMAND = "trendmicro-visionone-collect-forensic-file"
-FILE_TO_SANDBOX_COMMAND = "trendmicro-visionone-submit-file-to-sandbox"
-CHECK_TASK_STATUS_COMMAND = "trendmicro-visionone-check-task-status"
 FETCH_INCIDENTS_COMMAND = "fetch-incidents"
-UPDATE_STATUS_COMMAND = "trendmicro-visionone-update-status"
+ON_POLL_COMMAND = "trendmicro-visionone-on-poll"
 ADD_NOTE_COMMAND = "trendmicro-visionone-add-note"
+STATUS_CHECK_COMMAND = "trendmicro-visionone-status-check"
+UPDATE_STATUS_COMMAND = "trendmicro-visionone-update-status"
+START_ANALYSIS_COMMAND = "trendmicro-visionone-start-analysis"
+ADD_BLOCKLIST_COMMAND = "trendmicro-visionone-add-to-block-list"
+FORCE_SIGN_OUT_COMMAND = "trendmicro-visionone-sign-out-account"
+DELETE_EMAIL_COMMAND = "trendmicro-visionone-delete-email-message"
+COLLECT_FILE_COMMAND = "trendmicro-visionone-collect-forensic-file"
+ENABLE_USER_ACCOUNT_COMMAND = "trendmicro-visionone-enable-account"
+TERMINATE_PROCESS_COMMAND = "trendmicro-visionone-terminate-process"
+QUARANTINE_DEVICE_COMMAND = "trendmicro-visionone-quarantine-device"
+GET_ENDPOINT_INFO_COMMAND = "trendmicro-visionone-get-endpoint-info"
+CHECK_TASK_STATUS_COMMAND = "trendmicro-visionone-check-task-status"
+TEST_CONNECTIVITY_COMMAND = "trendmicro-visionone-test-connectivity"
+DISABLE_USER_ACCOUNT_COMMAND = "trendmicro-visionone-disable-account"
+FORENSIC_FILE_INFO_COMMAND = "trendmicro-visionone-forensic-file-info"
+GET_EXCEPTION_LIST_COMMAND = "trendmicro-visionone-get-exception-list"
+FILE_TO_SANDBOX_COMMAND = "trendmicro-visionone-submit-file-to-sandbox"
+GET_SUSPICIOUS_LIST_COMMAND = "trendmicro-visionone-get-suspicious-list"
+UNQUARANTINE_DEVICE_COMMAND = "trendmicro-visionone-unquarantine-device"
+REMOVE_BLOCKLIST_COMMAND = "trendmicro-visionone-remove-from-block-list"
+FORCE_PASSWORD_RESET_COMMAND = "trendmicro-visionone-force-password-reset"
+QUARANTINE_EMAIL_COMMAND = "trendmicro-visionone-quarantine-email-message"
+ADD_EXCEPTION_LIST_COMMAND = "trendmicro-visionone-add-objects-to-exception-list"
+ADD_SUSPICIOUS_LIST_COMMAND = "trendmicro-visionone-add-objects-to-suspicious-list"
+DELETE_EXCEPTION_LIST_COMMAND = "trendmicro-visionone-delete-objects-from-exception-list"
+GET_SANDBOX_ANALYSIS_RESULTS_COMMAND = "trendmicro-visionone-get-sandbox-analysis-results"
+DELETE_SUSPICIOUS_LIST_COMMAND = "trendmicro-visionone-delete-objects-from-suspicious-list"
+GET_SANDBOX_SUBMISSION_STATUS_COMMAND = "trendmicro-visionone-get-sandbox-submission-status"
+DOWNLOAD_INVESTIGATION_PACKAGE_COMMAND = "trendmicro-visionone-download-investigation-package"
+DOWNLOAD_SUSPICIOUS_OBJECT_LIST_COMMAND = "trendmicro-visionone-download-suspicious-object-list"
+DOWNLOAD_SANDBOX_ANALYSIS_REPORT_COMMAND = "trendmicro-visionone-download-sandbox-analysis-report"
